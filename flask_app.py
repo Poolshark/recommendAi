7c4a837dd0aeb758f86d4a1cbc544d17e9a8e2ca
Flo Ragossnig <flo@codeversity.com>
2025-02-06 16:39:41 +0100

    Testing integration

--- flask_app.py
+++ flask_app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask! - test with Git'
+    return 'Hello from Flask! - test with Git INTEG'
 
 
 @app.route('/git_update', methods=['POST'])
